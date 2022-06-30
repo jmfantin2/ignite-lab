2b6d9da4ef5cc1b452e12b9ac5f98982a3f1ba80
JM Fantin <jmfantin@outlook.com>
2022-06-30 01:38:32 -0300

    lessons from graphCMS

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,53 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+// FRONTEND: type the damn data
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
+  console.log(data);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
       <div className="flex flex-col gap-8">
-        <Lesson
-          title="Aula 01"
-          slug="aula-01"
-          availableAt={new Date()}
-          type="class"
-        />
+        {data?.lessons.map((l) => {
+          return (
+            <Lesson
+              key={l.id}
+              title={l.title}
+              slug={l.slug}
+              availableAt={new Date(l.availableAt)}
+              type={l.lessonType}
+            />
+          );
+        })}
       </div>
     </aside>
   );
 }
+
+// BACKEND: serve the damn data
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      slug
+      title
+    }
+  }
+`;
